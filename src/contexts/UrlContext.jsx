7403c1ea31a5e0f7a58648cda23e9ca8a300e844
CarloMart88 <carlo.martino1988@gmail.com>
2025-11-26 11:29:36 +0100

    feat dataFetching into the provider

--- src/contexts/UrlContext.jsx
+++ src/contexts/UrlContext.jsx
@@ -1,9 +1,18 @@
 import { createContext, useContext, useEffect, useState } from "react";
+const baseUrl = process.env.REACT_APP_URL;
 //Creare un Context API (GlobalContext) per gestire lo stato globale dell'applicazione.
 const UrlContext = createContext();
 
 function UrlProvider({ children }) {
   const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetch(`${baseUrl}/tasks`)
+      .then((res) => res.json())
+      .then((data) => {
+        setTasks(data);
+        console.log(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 }
