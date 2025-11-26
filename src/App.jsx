f8d473cd60bf61c923d9cbd107d6dca742646844
CarloMart88 <carlo.martino1988@gmail.com>
2025-11-26 10:44:54 +0100

    feat insert comment

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TaskList from "./pages/TaskList";
 import AddTask from "./pages/AddTask";
 import DefaultLayout from "./Layout/DefaultLayout";
+/**Definiamo due pagine principali:
+
+Lista dei Task (TaskList.jsx) → mostrerà l'elenco dei task.
+Aggiungi Task (AddTask.jsx) → conterrà il form per aggiungere un nuovo task. */
 function App() {
   return (
     <>
@@ -14,6 +18,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
+    /**Definire le rotte con Routes e Route, associando ogni percorso alla rispettiva pagina. */
   );
 }
 
--- src/Layout/DefaultLayout.jsx
+++ src/Layout/DefaultLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
-
+/**Aggiungere una barra di navigazione con NavLink, per permettere all'utente di spostarsi tra le pagine. */
 function DefaultLayout() {
   return (
     <>
