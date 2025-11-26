aa41ab48e2c40950530c6037417048599ad2d439
CarloMart88 <carlo.martino1988@gmail.com>
2025-11-26 17:43:14 +0100

    feat flex navBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,18 +3,20 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="col-12">
-      <nav>
-        <div>
-          <NavLink to="/TaskList">TaskList</NavLink>
-        </div>
-        <div>
-          <NavLink to="./AddTask">AddTask</NavLink>
-        </div>
-        <div>
-          <NavLink to="./">HomePage</NavLink>
-        </div>
-      </nav>
+    <div className="col-12 my-2">
+      <div className="row ">
+        <nav className="d-flex flex-row">
+          <div className="m-2">
+            <NavLink to="/TaskList">TaskList</NavLink>
+          </div>
+          <div className="m-2">
+            <NavLink to="./AddTask">AddTask</NavLink>
+          </div>
+          <div className="m-2">
+            <NavLink to="./">HomePage</NavLink>
+          </div>
+        </nav>
+      </div>
     </div>
   );
 }
