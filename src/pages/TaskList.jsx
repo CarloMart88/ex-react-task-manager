7a1c1fcce71565f31054080c0c6187a0a8e30c0f
CarloMart88 <carlo.martino1988@gmail.com>
2025-11-26 12:22:43 +0100

    feat new Tabel vision

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import { useUrl } from "../contexts/UrlContext";
-
+// Milestone 3 - Lista dei Task (Pagina)
 function TaskList() {
+  //Recuperare la lista dei task dal GlobalContext e mostrarla nella pagina TaskList.jsx.
+  const [tasks, setTasks] = useUrl();
   return (
     <div>
-      <h1>TaskList</h1>
+      {/**Strutturare TaskList.jsx come una tabella, con le intestazioni Nome, Stato, Data di Creazione. */}
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Stato</th>
+            <th scope="col">Data di Creazione</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
