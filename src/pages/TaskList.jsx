f16d7ed9be34a7ad1f9d49529282430676ec033a
CarloMart88 <carlo.martino1988@gmail.com>
2025-12-03 10:38:47 +0100

    feat debounce function

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -7,6 +7,17 @@ function TaskList() {
   const { tasks, setTasks } = useUrl();
   const [sortBy, setSortBy] = useState("createdAt");
   const [sortOrder, setSortOrder] = useState(1);
+  const [searchQuery, setSearchQuery] = useState("");
+  // creo una funzione di debounce
+  function debounce(callback, delay) {
+    let timer;
+    return (value) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        callback(value);
+      }, delay);
+    };
+  }
 
   const handleSort = (field) => {
     if (sortBy === field) {
@@ -15,30 +26,27 @@ function TaskList() {
       setSortBy(field);
       setSortOrder(1);
     }
+    console.log("questo è il valore di", sortBy);
   };
 
   const sortedTask = useMemo(() => {
-    return [...tasks].sort((a, b) => {
-      let comparison;
-      if (sortBy === "title") {
-        comparison = a.title.localeCompare(b.title);
-      } else if (sortBy === "status") {
-        const Possibilty = ["To do", "Doing", "Done"];
-        comparison =
-          Possibilty.indexOf(a.status) - Possibilty.indexOf(b.status);
-      } else if (sortBy === "createdAt") {
-        const newDateA = new Date(a.createdAt).getTime();
-        const newDateB = new Date(b.createdAt).getTime();
-        comparison = newDateA - newDateB;
-      }
-
-      return comparison * sortOrder;
-    });
-  }, [sortBy, sortOrder, tasks]);
+    if (searchQuery === "") {
+      return tasks;
+    } else {
+      return tasks.filter((t) =>
+        t.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+  }, [searchQuery, sortBy, sortOrder, tasks]);
 
   return (
     <div>
       {/**Strutturare TaskList.jsx come una tabella, con le intestazioni Nome, Stato, Data di Creazione. */}
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
       <table className="table">
         <thead>
           <tr>
